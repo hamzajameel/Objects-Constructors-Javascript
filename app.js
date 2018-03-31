a3eab7eb419af63e827ffc97400213de535cf424
hamzajameel <prince24780@gmail.com>
2018-03-31 15:23:03 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+function calcar(name, price, space, ) {
+     this.name = name;
+    this.price = price;
+     this.space = space;
+    
+     }
+
+     var car1 = new calcar("Benze", 3.99, 100, );
+ var car2 = new calcar("Duel", 5.99 , 500, );
+ var car3 = new calcar("Ultimate", 9.99, 2000, );
+
+console.log(car1);
+console.log(car2);
+console.log(car3);
+
